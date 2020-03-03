ea9bcfe68b81b6c3915880bbeb5feef9fb76df43
doryunnim <57827098+doryunnim@users.noreply.github.com>
2020-03-03 18:40:57 +0900

    Add files via upload

---
+++ konlpyTest.py
@@ -0,0 +1,8 @@
+from konlpy.tag import Twitter
+twitter = Twitter()
+print(twitter.morphs(u'단독입찰보다 복수입찰의 경우'))
+print(twitter.nouns(u'유일하게 항공기 체계 종합개발 경험을 갖고 있는 KAI는'))
+print(twitter.phrases(u'날카로운 분석과 신뢰감 있는 진행으로'))
+print(twitter.pos(u'이것도 되나욬ㅋㅋ'))
+print(twitter.pos(u'이것도 되나욬ㅋㅋ', norm=True))
+print(twitter.pos(u'이것도 되나욬ㅋㅋ', norm=True, stem=True))
